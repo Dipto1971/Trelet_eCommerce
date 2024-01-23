e20193302c4c304d642910ee6bbff984e311ebed
Dipto1971 <dipto.haq771@gmail.com>
2024-01-23 17:07:54 +0600

    Token system updated

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '15d'
+        expiresIn: '1d'
     });
 
     // Set JWT as HTTP-only cookie
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
-        maxAge: 1000 * 60 * 60 * 24 * 15 // 15 days
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
     });
     // res.cookie allows us to set a cookie in the browser which will be sent to the server with every request and match with the JWT secret to authenticate the user. If the user is authenticated, the user will be logged in.
 }
